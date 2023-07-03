3cd254de66ab41d1719fc8fb733c1b0a2a86e2ed
Tasnim Jamil Alshair <90467107+tasnimalshair@users.noreply.github.com>
2023-07-03 17:18:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const countries = JSON.parse(fs.readFileSync("countries.json", "utf-8"));
+
+const app = (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const searchedCountry = url.searchParams.get("searchedCountry");
+
+  switch (url.pathname) {
+    case "/searchForCountry":
+      if (!searchedCountry) {
+        res.end("Bad request");
+      } else {
+        const similarCountries = countries.filter((word) =>
+          word.toLowerCase().startsWith(searchedCountry.toLowerCase())
+        );
+
+        res.end(JSON.stringify(similarCountries));
+      }
+      break;
+    default:
+      res.end("No url founded.");
+  }
+};
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const app = require("./app");
+const port = 5050;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`SERVER is running on http://localhost:${port}`);
+});
